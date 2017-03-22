fe113ffbb63b70c1a22b7b778159e9e774f15004
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-03-22 16:47:57 +0100

    added printout and uppercase main colors to Colors file

--- week2/day3/Colors.java
+++ week2/day3/Colors.java
@@ -1,4 +1,6 @@
 import javax.sound.midi.Soundbank;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,24 +10,35 @@ public class Colors {
 
     public static void main(String[] argc) {
 
-        String[][] color = new String[3][5];
+        int numOfMainColor = 3;
+        int numOfMaxShades = 6;
+
+        String[][] color = new String[numOfMainColor][numOfMaxShades];
 
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < numOfMainColor; i++) {
             Scanner input = new Scanner(System.in);
-            System.out.println("Give me a basic color please: ");
+            System.out.println("Give me a main color please: ");
             String mainColor = input.nextLine();
-            color[i][0] = mainColor;
+            color[i][0] = mainColor.toUpperCase() + ":";
 
-            for (int j = 0; j < 5; j++) {
+            for (int j = 1; j < numOfMaxShades; j++) {
                 System.out.println("Give me a shade of the color: ");
                 Scanner shades = new Scanner(System.in);
                 String shade = shades.nextLine();
-                color[i][j] = shade;
+                color[i][j] = shade + ", ";
             }
+        }
+
+        for (int n = 0; n < 3; n++) {
+            for (int g = 0; g < 5; g++) {
+                System.out.print(color[n][g] + " ");
+            }
+            System.out.println();
         }
     }
 }
+
 
 // - Create a two dimensional array
 //   which can contain the different shades of specified colors
