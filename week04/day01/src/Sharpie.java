510365ee087c882b92e9f0e1bac8d0d99a4fc9ad
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-04-03 14:46:34 +0200

    add completed Sharpie class

---
+++ week04/day01/src/Sharpie.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Gabor on 03/04/2017.
+ */
+public class Sharpie {
+
+  String color;
+  float width;
+  float inkAmount;
+
+  public Sharpie( String color, float width) {
+    this.inkAmount = 100;
+  }
+
+  public void use() {
+    this.inkAmount -= 2;
+  }
+
+}
