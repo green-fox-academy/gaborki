02d9eabb34f1e788a6f1ab59493d6fe4469142c0
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-04-04 15:28:12 +0200

    create Aircraft class' functions

--- week04/day02/src/AircraftCarrier/Aircraft.java
+++ week04/day02/src/AircraftCarrier/Aircraft.java
@@ -4,38 +4,34 @@ package AircraftCarrier;
  * Created by Gabor on 04/04/2017.
  */
 public class Aircraft {
+
   int ammoStore;
-  private int maxAmmo;
-  private int baseDamage;
+  int maxAmmo;
+  int baseDamage;
 
-  public int fight(){
+  public int fight() {
     this.ammoStore = 0;
-    return
+    return this.baseDamage * this.ammoStore;
   }
 
-  public void refill(int ammoToRefill){
-    if (maxAmmo - ammoStore > ammoToRefill ){
-      this.ammoStore =+ ammoToRefill;
-  }
-  else{
+  public void refill(int ammoToRefill) {
+    if (maxAmmo - ammoStore > ammoToRefill) {
+      this.ammoStore = +ammoToRefill;
+    } else {
       this.ammoStore = maxAmmo;
     }
   }
 
-  public String get_type(){
-    if (Aircraft instanceof F16){
+  public String get_type(Aircraft aircraft) {
+    if (aircraft instanceof F16) {
       return "F16";
-    }
-    else{
+    } else {
       return "F35";
     }
   }
 
-  public String get_status(){
-
+  public String get_status(Aircraft aircraft) {
+    return "Type: " + get_type(aircraft) + ", Ammo: " + aircraft.ammoStore + ", Base Damage: "
+        + aircraft.baseDamage + ", Alldamage: " + aircraft.ammoStore * aircraft.baseDamage;
   }
 }
-
-
-
-
