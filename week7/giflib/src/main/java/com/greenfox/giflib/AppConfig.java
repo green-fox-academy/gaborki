10ce20cdb440c5051e4e93afbe3cd0e6bc18d007
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-05-01 15:16:57 +0200

    setup and run empty giflib App with Spring in Gradle

---
+++ week7/giflib/src/main/java/com/greenfox/giflib/AppConfig.java
@@ -0,0 +1,13 @@
+package com.greenfox.giflib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class AppConfig {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppConfig.class, args);
+  }
+
+}
