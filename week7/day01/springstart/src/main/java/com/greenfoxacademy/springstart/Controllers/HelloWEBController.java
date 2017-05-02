db64eb54fef3f0b351e7ea7427cd39d9d22871ee
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-05-02 15:27:20 +0200

    helloRESTController wroks fine

--- week7/day01/springstart/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWEBController.java
+++ week7/day01/springstart/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWEBController.java
@@ -1,21 +1,16 @@
 package com.greenfoxacademy.springstart.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by Gabor on 02/05/2017.
- */
 
-@RestController
+@Controller
 public class HelloWEBController {
 
   @RequestMapping("/web/greeting")
-  @ResponseBody
-  public Model greeting(Model model) {
+  public String greeting(Model model) {
     model.addAttribute("name", " World");
-    return model;
+    return "greeting";
   }
 }
