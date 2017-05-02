5f74567ce6d8e0d392b99e43e6e2635a4c12b02c
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-05-02 17:42:56 +0200

    create random language greeting, prints one radom greeting with random color and fontsize

--- week7/day01/springstart/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWEBController.java
+++ week7/day01/springstart/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWEBController.java
@@ -14,8 +14,40 @@ public class HelloWEBController {
 
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam ("yourName") String yourName) {
+    model.addAttribute("helloo", getRandomHello());
     model.addAttribute("name", yourName);
     model.addAttribute("countIt", count.getAndIncrement());
+    model.addAttribute("colorIt", getRandomColor());
+    model.addAttribute("fontsizeIt", getRandomFontSize());
     return "greeting";
   }
+
+  public String getRandomColor()
+  {
+    String[] letters = new String[15];
+    letters = "0123456789ABCDEF".split("");
+    String code ="#";
+    for(int i=0;i<6;i++)
+    {
+      double ind = Math.random() * 15;
+      int index = (int)Math.round(ind);
+      code += letters[index];
+    }
+    return code;
+  }
+
+  public int getRandomFontSize(){
+    int font = (int)(Math.random()*100+15);
+    return font;
+  }
+
+  public String getRandomHello(){
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+        "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+        "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+        "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    String helloo = hellos[(int)(Math.random()*(hellos.length-1))];
+    return helloo;
+  }
+
 }
